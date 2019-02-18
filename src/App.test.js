0ba89dc51cb89793ba05d9523ac4668823c36610
mdashmiller <m.robert.miller@gmail.com>
2019-02-18 16:31:23 -0600

    initial setup

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+import Enzyme, { shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+
+import { findByTestAttr } from '../Utils'
+
+Enzyme.configure({ adapter: new Adapter() })
+
+// render component for testing
+const setUp = (props={}) => {
+  return shallow(<App {...props} />)
+}
+
+describe('App rendering', () => {
+
+  let component
+  beforeAll(() => {
+    component = setUp()
+  })
+
+  it('should render 1 h1', () => {
+    const wrapper = findByTestAttr(component, 'title')
+    expect(wrapper.length).toBe(1)
+  })
+})
+
+describe('App mounting and unmounting', () => {
+
+  it('should render without error', () => {
+    const div = document.createElement('div')
+    ReactDOM.render(<App />, div)
+    ReactDOM.unmountComponentAtNode(div)
+  })
+})
